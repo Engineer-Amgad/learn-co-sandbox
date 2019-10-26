832ae3d2657abcd282a3f38dd5019a6802dd6892
Amgad Malik <amgadmalik@yahoo.com>
2019-10-26 13:29:40 +0000

    first commit

---
+++ greenville-schools-cli/lib/greenville/schools/cli.rb
@@ -0,0 +1,10 @@
+require "greenville/schools/cli/version"
+
+module Greenville
+  module Schools
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ greenville-schools-cli/lib/greenville/schools/cli/version.rb
@@ -0,0 +1,7 @@
+module Greenville
+  module Schools
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end
